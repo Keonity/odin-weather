6dd4c51129293fce6a7ebbad0f42037c2913a33c
Keonity <56704285+Keonity@users.noreply.github.com>
2024-08-22 04:18:18 -0700

    Added basic weather searching functionality.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import './styles.css';
 import testBg from './coffeeBg.jpg';
 
-const image = document.createElement("img");
-image.src = testBg;
-
-document.body.appendChild(image);
-
 console.log(`Welcome To The Eye Of Odin Weather Project`);
+
+const resultHeader = document.querySelector(`#resultHeader`);
+const searchbar = document.querySelector(`#searchbar`);
+const submit = document.querySelector(`#submit`);
+
+const queryLocation = `Torrance`;
+const query = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${queryLocation}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`;
+
+async function getWeather(data = `Torrance`) {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`);
+    const weatherData = await response.json();
+    console.log(weatherData.currentConditions.temp);
+    resultHeader.innerHTML = `The temperature in ${weatherData.resolvedAddress} is ${weatherData.currentConditions.temp} degrees Celsius`;
+    return weatherData;
+}
+
+submit.addEventListener("click", () => {
+    getWeather(searchbar.value);
+});
+
+getWeather();
