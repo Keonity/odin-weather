ec51a9ffdfe9e5283a72d8ee48921857bd04c6ee
Keonity <56704285+Keonity@users.noreply.github.com>
2024-08-22 18:28:40 -0700

    Finished assignment per Odin Project specifications, added animation and data display on page.

--- src/index.js
+++ src/index.js
@@ -4,18 +4,37 @@ import testBg from './coffeeBg.jpg';
 console.log(`Welcome To The Eye Of Odin Weather Project`);
 
 const resultHeader = document.querySelector(`#resultHeader`);
+const description = document.querySelector(`#description`);
 const searchbar = document.querySelector(`#searchbar`);
 const submit = document.querySelector(`#submit`);
+const loadAnimation = document.querySelector(`#loadAnimationOff`);
 
-const queryLocation = `Torrance`;
-const query = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${queryLocation}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`;
+const queryLocation = `Las Vegas`;
 
-async function getWeather(data = `Torrance`) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`);
-    const weatherData = await response.json();
-    console.log(weatherData.currentConditions.temp);
-    resultHeader.innerHTML = `The temperature in ${weatherData.resolvedAddress} is ${weatherData.currentConditions.temp} degrees Celsius`;
-    return weatherData;
+async function getWeather(data = `Las Vegas`) {
+    try {
+        loadAnimation.setAttribute(`id`, `loadAnimation`);
+        let response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${data}?unitGroup=metric&key=5MGVRN4YAV8UJY9MGZB77KHLE&contentType=json`);
+        if (!response.ok) {
+            throw new Error(response.status)
+        }
+        let weatherData = await response.json();
+        loadAnimation.setAttribute(`id`, `loadAnimationOff`)
+        console.log(weatherData.currentConditions.temp);
+        resultHeader.innerHTML = `The temperature in ${weatherData.resolvedAddress} is ${weatherData.currentConditions.temp}` + `&#8451`;
+        description.innerHTML = `${weatherData.description}`
+    }
+    catch (error) {
+        if (error.message === `400`) {
+            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
+            resultHeader.innerHTML = `Error ${error.message}: Invalid search parameters, please try again with a different location.`;
+        }
+        else {
+            loadAnimation.setAttribute(`id`, `loadAnimationOff`);
+            resultHeader.innerHTML = `Error ${error.message}`;
+        }
+        description.innerHTML = ``;
+    }
 }
 
 submit.addEventListener("click", () => {
